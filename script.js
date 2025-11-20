ed99e535e223e1aea1b54c4bb6656783479fc710
Likhon <likhon.929@gmail.com>
2025-11-21 00:03:03 +0600

    Update title and download functionality for CV

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.getElementById("downloadBtn").addEventListener("click", function () {
     // Replace with your actual PDF file name
-    const fileUrl = "cv.pdf"; 
+    const fileUrl = "./Resume of Jubayer Alam Likhon.pdf"; 
     
     const link = document.createElement("a");
     link.href = fileUrl;
-    link.download = "Jubayer-CV.pdf";
+    link.download = "./Resume of Jubayer Alam Likhon.pdf";
     link.click();
 });
